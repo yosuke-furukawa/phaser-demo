36f0bb3014e9f4190c1f389151b69c37f2f3a82e
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2014-07-08 20:27:50 +0900

    Add animation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(process.PORT || 3000);
 var io = require("socket.io")(server);
 
 console.log(__dirname + "../client/src");
@@ -8,6 +8,12 @@ app.use(express.static(__dirname + "/../client/src"));
 
 
 io.on("connection", function(socket){
+  socket.on("animation", function(animation) {
+    socket.broadcast.emit("animation", {
+      id: socket.id,
+      animation : animation
+    });
+  });
   socket.on("position", function(position) {
     socket.broadcast.emit("position", {
       id: socket.id,
